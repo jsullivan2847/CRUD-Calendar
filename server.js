3ba9633cc7b3ce94f98e05e1e8007191f4654c9a
James Sullivan <jsullivan2847@gmail.com>
2022-05-27 16:00:23 -0400

    set up express app and connected mongo

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const session =  require('express-session');
-require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', (error) => console.log("mongo error", error));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+app.get('/', (req,res) => {
+    res.send('hello');
+});
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log('express is listening at ', PORT));
